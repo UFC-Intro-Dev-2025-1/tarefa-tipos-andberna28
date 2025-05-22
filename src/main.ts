5f347fe1336bb6fc707359ee852c63d42b864e8d
Anderson <118529145+andberna28@users.noreply.github.com>
2025-05-22 20:17:17 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 // Escreva seu código aqui!
+
+let professora: string = "Lana";
+const cadeira: number = 40;
+let temAlunos: boolean = true;
+const cabos: undefined = undefined;
+
+interface Computador {
+    monitor: string;
+    teclado: string;
+    quantidade: number;
+}
+
+let computadores: Computador = {
+    monitor: "DELL",
+    teclado: "MYMAX",
+    quantidade: 30,
+};
+
+let resultadoFinal = professora + '\n' + cadeira + '\n' + temAlunos + '\n' + cabos + '\n' + computadores.monitor + '\n' + computadores.teclado + '\n' + computadores.quantidade;
+
+console.log(resultadoFinal)
